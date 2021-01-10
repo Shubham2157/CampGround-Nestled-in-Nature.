7c83118b3fc477c8dbd4a5d3e93837c4dde4fcd2
Shubham Kumar Jha <kumarshubhamjha2157@gmail.com>
2021-01-10 10:38:36 +0530

    seed DB (added data) for more fun...

--- app.js
+++ app.js
@@ -28,19 +28,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // setting view engine to ejs
 app.set("view engine", "ejs")
 
-// var campgrounds = [
-//     { name: "Shubham Jha", image: "https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e507748752e7ed1974bc2_340.jpg" },
-//     { name: "Preet Anurag", image: "https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852577049742a7ed4914a_340.jpg" },
-//     { name: "Shubham Jha", image: "https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e507748752e7ed1974bc2_340.jpg" },
-//     { name: "Preet Anurag", image: "https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852577049742a7ed4914a_340.jpg" },
-//     { name: "Shubham Jha", image: "https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e507748752e7ed1974bc2_340.jpg" },
-//     { name: "Preet Anurag", image: "https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852577049742a7ed4914a_340.jpg" },
-//     { name: "Shubham Jha", image: "https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e507748752e7ed1974bc2_340.jpg" },
-//     { name: "Preet Anurag", image: "https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852577049742a7ed4914a_340.jpg" },
-//     { name: "Vinayak Agnihotri", image: "https://pixabay.com/get/55e8dc404f5aab14f1dc84609620367d1c3ed9e04e507748752e7ed1974bc2_340.jpg" },
-//     { name: "Vinayak Agnihotri", image: "https://pixabay.com/get/55e8dc404f5aab14f1dc84609620367d1c3ed9e04e507748752e7ed1974bc2_340.jpg" },
-//     { name: "Vinayak Agnihotri", image: "https://pixabay.com/get/55e8dc404f5aab14f1dc84609620367d1c3ed9e04e507748752e7ed1974bc2_340.jpg" }
-// ]
 
 // Home Page Route
 app.get("/", (req, res) => {
--- seeds.js
+++ seeds.js
@@ -1,12 +1,49 @@
 const mongoose = require("mongoose")
 const Campground = require("./models/campground")
 
-function seedDB(){
+var data = [
+    {
+        name: "Shubham Jha",
+        image: "https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e507748752e7ed1974bc2_340.jpg",
+        description: "Blah Blah Blahajhk jhksd"
+    },
+    {
+        name: "Preet Anurag",
+        image: "https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852577049742a7ed4914a_340.jpg",
+        description: "Blah Blah Blahajhk jhksd"
+    },
+    // { name: "Shubham Jha", image: "https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e507748752e7ed1974bc2_340.jpg" },
+    // { name: "Preet Anurag", image: "https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852577049742a7ed4914a_340.jpg" },
+    // { name: "Shubham Jha", image: "https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e507748752e7ed1974bc2_340.jpg" },
+    // { name: "Preet Anurag", image: "https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852577049742a7ed4914a_340.jpg" },
+    // { name: "Shubham Jha", image: "https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e507748752e7ed1974bc2_340.jpg" },
+    // { name: "Preet Anurag", image: "https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf852577049742a7ed4914a_340.jpg" },
+    // { name: "Vinayak Agnihotri", image: "https://pixabay.com/get/55e8dc404f5aab14f1dc84609620367d1c3ed9e04e507748752e7ed1974bc2_340.jpg" },
+    // { name: "Vinayak Agnihotri", image: "https://pixabay.com/get/55e8dc404f5aab14f1dc84609620367d1c3ed9e04e507748752e7ed1974bc2_340.jpg" },
+    {
+        name: "Vinayak Agnihotri",
+        image: "https://pixabay.com/get/55e8dc404f5aab14f1dc84609620367d1c3ed9e04e507748752e7ed1974bc2_340.jpg",
+        description: "Blah Blah Blahajhk jhksd"
+    }
+]
+
+function seedDB() {
+    // Remove all campgrounds
     Campground.remove({}, (err) => {
-        if(err){
+        if (err) {
             console.log(err);
-        } else{
+        } else {
             console.log("remove campground!");
+            // add new campgrounds 
+            data.forEach((seed) => {
+                Campground.create(seed, (err, data) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("Added a campground");
+                    }
+                })
+            })
         }
     })
 }
